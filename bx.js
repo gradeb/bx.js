1d459e3570423ec0c159241bb8d430d61ee65bd9
Harsh Rajput <32320131+gradeb@users.noreply.github.com>
2019-06-18 18:26:33 +0530

    removed on and off functions
    no need of on() or off() function, go vanilla!

--- bx.js
+++ bx.js
@@ -1,10 +1,6 @@
 /* bx.js | India | MIT */
 
-export const on = ( a, b, c ) => a.addEventListener( b, c );
-
-export const off = ( a, b, c ) => a.removeEventListener( b, c );
-
-export const ready = ( a, b ) => a.readyState !== "loading" ? setTimeout( b, 0 ) : on( a, 'DOMContentLoaded', b );
+export const ready = ( a, b ) => a.readyState !== "loading" ? setTimeout( b, 0 ) : a.addEventListener( 'DOMContentLoaded', b );
 
 export const select = ( a, b = document ) => b.querySelector( a );
 
@@ -12,4 +8,4 @@ export const selectAll = ( a, b = document ) => b.querySelectorAll( a );
 
 export const create = a => document.createElement( a );
 
-export default { on, off, ready, select, selectAll, create };
+export default { ready, select, selectAll, create };
